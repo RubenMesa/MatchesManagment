a2099a945881085f3540ba037f19b5c85df78206
RubenMesa <ruben.mesa.up@gmail.com>
2022-11-16 15:57:43 -0300

    subida clases simples 1

---
+++ Cancha.py
@@ -0,0 +1,9 @@
+class Cancha:
+    def __init__(self, codigo, nombre, tamano, ubicacion) -> None:
+        self.codigo = codigo
+        self.nombre = nombre
+        self.tamano = tamano
+        self.ubicacion= ubicacion
+
+
+   
---
+++ Usuario.py
@@ -0,0 +1,17 @@
+class Usuario :
+    
+    pieder = False 
+    pieizq = False
+    #CONSTRUCTOR
+    def __init__(self, nombre, user, password, poscision, pieder, pieizq ) -> None:
+        self.nombre = nombre
+        self.user = user
+        self.password =  password
+        self.poscision = poscision
+        self.pieder = pieder
+        self.pieizq = pieizq
+        
+
+
+    
+
