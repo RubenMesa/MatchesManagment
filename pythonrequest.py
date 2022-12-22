ddd9a2b940ded792f1f349975519999deae2b2fb
marcogodoya <marco.godoy14@inacapmail.cl>
2022-12-21 21:39:38 -0300

    api7

--- pythonrequest.py
+++ pythonrequest.py
@@ -1,19 +1,32 @@
 import requests
 
-class PythonRequest():
+res = requests.get('http://127.0.0.1:8000/api/posiciones/')
 
-    def __init__(self, url):
-        self.url=url
+print(res)
+json = res.json()
+print(json['verposicion'])
+for element in json['verposicion']:
+    #for j in element['nombre']:
+    print(element['nombre']," - ",element['descripcion'])
+    #print(element)
+    #jsonkeys= json.keys()
+    #if element == 'verposicion':
+        #for i in element: 
+            #print(i.values())
+#class PythonRequest():
 
-    def agregar(self, data):
-        response = requests.post(self.url, data)
-        respuesta_json = response.json()
-        print(respuesta_json)
+ #   def __init__(self, url):
+  #      self.url=url
+
+   # def agregar(self, data):
+    #    response = requests.post(self.url, data)
+     #   respuesta_json = response.json()
+      #  print(respuesta_json)
     
-agrega= PythonRequest('http://127.0.0.1:8000/api/posiciones/')
-data = {
-    "nombre":"mediapunta",
-    "descripcion":"Dar el último pase a los delanteros para el gol"
-}
+#agrega= PythonRequest('http://127.0.0.1:8000/api/posiciones/')
+#data = {
+ #   "nombre":"mediapunta",
+  #  "descripcion":"Dar el último pase a los delanteros para el gol"
+#}
 
-agrega.agregar(data)
+#agrega.agregar(data)
